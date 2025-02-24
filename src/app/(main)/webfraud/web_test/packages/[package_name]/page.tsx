03deaa067f43a500ba5d1158727208454a4713a0
harsh-mfilterit <harsh.yadav@mfilter.com>
2025-02-24 12:05:44 +0530

    tracker page update

--- src/app/(main)/webfraud/web_test/packages/[package_name]/page.tsx
+++ src/app/(main)/webfraud/web_test/packages/[package_name]/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import { TRACKER } from "../../DATA";
 import { Button } from "@/components/ui/button";
 import ItemTable from "../ItemTable";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+import Link from "next/link";
+
 export default function ListTrackers() {
   const packageName = useParams().package_name;
   const extrafields = [
@@ -22,22 +24,32 @@ export default function ListTrackers() {
     },
   ];
 
-  const nav = useRouter();
-  return (
-    <div className="py-2 px-8">
+  function renderHeader() {
+    return (
       <div className="px-8 py-5 bg-white rounded-xl flex items-center justify-between">
-        <h2 className="text-2xl font-medium text-gray-900 capitalize">
-          Package Trackers for {packageName}
+        <h2 className="text-xl font-medium text-gray-900 capitalize">
+          Package Trackers
         </h2>
         <div>
-          <Button className="px-6 py-2 rounded-full capitalize text-white bg-purple-400 hover:bg-purple-600">
+          <Link
+            href={`${packageName}/analytics`}
+            className="px-6 py-2 rounded-full capitalize text-white bg-purple-400 hover:bg-purple-600"
+          >
             analytics
-          </Button>
-          <Button className="px-6 py-2 rounded-full capitalize text-white ml-9 bg-purple-400 hover:bg-purple-600">
+          </Link>
+          <Link
+            href={`${packageName}/create_tracker`}
+            className="px-6 py-2 rounded-full capitalize text-white ml-9 bg-purple-400 hover:bg-purple-600"
+          >
             create tracker
-          </Button>
+          </Link>
         </div>
       </div>
+    );
+  }
+  return (
+    <div className="py-2 px-8">
+      {renderHeader()}
       <div className=" w-full px-3 py-2 bg-white rounded-xl mt-5">
         <ItemTable
           data={TRACKER.filter((item) => item.package_name === packageName)}
